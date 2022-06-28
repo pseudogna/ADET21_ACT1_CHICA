aba6a0e3de6c718520c335ff48c876b2d3bc522d
pseudogna <108336797+pseudogna@users.noreply.github.com>
2022-06-28 18:38:25 +0800

    Add files via upload

---
+++ ADET21_ACT1_CHICA/bin/controller.dart
@@ -0,0 +1,110 @@
+import 'dart:io';
+import 'main.dart';
+
+class operation 
+{
+  readFile() 
+  {
+    var content = File('accountRecord.txt').readAsLinesSync();
+    return content;
+  }
+
+  //validates the pin
+  int accountValidation(var pin) 
+  {
+    var user = readFile().elementAt(0).split(' ');
+    var password = readFile().elementAt(1).split(' ');
+
+    if (password.elementAt(1) == pin) 
+    {
+      print("\n\n\t\tWelcome back " + user.elementAt(1) + "!");
+      print("\tPlease choose your transaction");
+      return 1;
+    } else 
+    {
+      print("\tAccount not found. Try again.");
+      return 2;
+    }
+  }
+
+  //depositing transaction
+  void depositOption() {
+    var balance = remainingBalance();
+    print('\n\n\t\tDEPOSIT');
+
+    stdout.write('\n\t>> AMOUNT TO DEPOSIT: P ');
+    var depositAmount =
+        (double.parse(stdin.readLineSync()!).toStringAsFixed(2));
+    var newBalance = double.parse(balance) + double.parse(depositAmount);
+    sinkTransaction(1, depositAmount, newBalance);
+    print('\t>> UPDATED BALANCE: P ' + newBalance.toStringAsFixed(2));
+
+    return menuSection();
+  }
+
+  //withdrawal transaction
+  void withdrawOption() 
+  {
+    var withdrawAmount;
+    var balance = remainingBalance();
+    print('\n\n\t\tWITHDRAW');
+
+    do {
+      stdout.write('\n\t>> AMOUNT TO WITHDRAW: P ');
+      withdrawAmount = (double.parse(stdin.readLineSync()!).toStringAsFixed(2));
+
+      if (double.parse(balance) == 0.00 ||
+          double.parse(withdrawAmount) == double.parse(balance) ||
+          double.parse(withdrawAmount) > double.parse(balance)) 
+          {
+        TransError();
+      } else {
+        break;
+      }
+    } while (true);
+
+    var newBalance = double.parse(balance) - double.parse(withdrawAmount);
+    sinkTransaction(2, withdrawAmount, newBalance);
+    return menuSection();
+  }
+
+  //checks balance transaction
+  void balanceOption() 
+  {
+    var balance = readFile().elementAt(readFile().length - 1).split(' ');
+    print('\n\n\t\tBALANCE INQUIRY');
+    balance = balance.elementAt(1);
+    print('\t>> BALANCE: P ' + balance);
+    return menuSection();
+  }
+
+  //appends the transaction to the file
+  void sinkTransaction(var transaction, var amount, var newBalance) 
+  {
+    var sink = File('accountRecord.txt'); 
+
+    if (transaction == 1) 
+    {
+      sink.writeAsStringSync(
+          '\n+ ' + amount + '\n ------- \n= ' + newBalance.toStringAsFixed(2),
+          mode: FileMode.append);
+    } else {
+      sink.writeAsStringSync(
+          '\n- ' + amount + '\n ------- \n= ' + newBalance.toStringAsFixed(2),
+          mode: FileMode.append);
+    }
+  }
+
+  //check the remaining balance
+  remainingBalance() 
+  {
+    var balance = readFile().elementAt(readFile().length - 1).split(' ');
+    balance = (double.parse(balance.elementAt(1)).toStringAsFixed(2));
+    return balance;
+  }
+
+  checkBalance(var amount) {}
+}
+
+ErrorMessage() => print("\tInput Invalid. \n\tTry again.");
+TransError() => print("\tThis exceeds the permitted amount. \n\tTry again.");
---
+++ ADET21_ACT1_CHICA/bin/main.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+import 'controller.dart';
+
+operation operate = new operation();
+
+void main() 
+{
+  var content = File('accountRecord.txt').readAsLinesSync();
+
+  userLogin(content);
+  menuSection();
+}
+
+//user login
+userLogin(var content) 
+{
+  var pin;
+  var val;
+  print('\n\n\t\tADET\n\tAUTOMATED TELLER MACHINE');
+
+  do {
+    stdout.write('\n>> Enter PIN (5 Digits): ');
+    pin = stdin.readLineSync();
+    val = operate.accountValidation(pin);
+     }
+  while (val == 2);
+}
+
+//menu
+menuSection() 
+{
+  var transaction;
+  printChoices show = printMenu;
+  show(operate);
+
+  do {
+    stdout.write('\n\t>> Transaction: ');
+    transaction = stdin.readLineSync();
+    if (int.parse(transaction) < 1 || int.parse(transaction) > 4) 
+    {
+      ErrorMessage();
+    }
+     } 
+
+  while (int.parse(transaction) < 1 || int.parse(transaction) > 4);
+
+  if (int.parse(transaction) == 1) 
+  {
+    operate.depositOption();
+  } else if (int.parse(transaction) == 2) 
+  {
+    operate.withdrawOption();
+  } else if (int.parse(transaction) == 3) 
+  {
+    operate.balanceOption();
+  } else {
+    print('\tLOGGING OUT\n\n');
+    return main();
+  }
+}
+
+//transaction options
+printMenu(operation operate) 
+{
+  print('\n\n\n\n\t ---- TRANSACTION ---- ');
+  print('\t|  [1] DEPOSIT        |');
+  print('\t|  [2] WITHDRAW       |');
+  print('\t|  [3] CHECK BALANCE  |');
+  print('\t|  [4] LOGOUT         |');
+  print('\t ---------------------');
+}
+
+typedef printChoices(operation operate);
+ErrorMessage() => print("\tInput Invalid. Try again.");
